8faa730abe0bbcf7ae341f3063a1c9f1fc2472b3
test <test@gmail.com>
2018-03-03 15:48:50 +0700

    Check space Main page

--- app/src/main/java/com/bird/qrfirebas/qrfirebase/frag/MainFragment.java
+++ app/src/main/java/com/bird/qrfirebas/qrfirebase/frag/MainFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bird.qrfirebas.qrfirebase.R;
+import com.bird.qrfirebas.qrfirebase.utility.MainAlert;
 
 public class MainFragment extends Fragment {
 
@@ -18,6 +21,33 @@ public class MainFragment extends Fragment {
 
 //        Register Controller
         registerController();
+
+//        Login Controller
+        loginController();
+    }
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText  = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    MainAlert mainAlert = new MainAlert(getActivity());
+                    mainAlert.normalDialog("Have Space","Plese Fill All Blank");
+                }else{
+
+                }
+
+
+
+            }
+        });
     }
 
     private void registerController() {
